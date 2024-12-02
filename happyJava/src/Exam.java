b4d1c1feab1478f32c2f144b12932f9d1157e3a0
byunicom <byunicom@gmail.com>
2024-12-02 14:48:58 +0900

    강의 자료 및 샘플 코드

--- happyJava/src/Exam.java
+++ happyJava/src/Exam.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Exam {
+    String name = "Kim Dong-Gyu";
+    String gender = "male";
+    String email ="byunicom@gmail.com";
+
+    public static void main(String[] args) {
+        Exam e = new Exam();
+        System.out.println("이름 : "+e.name+", 성별 : "+e.gender+", 이메일 : "+e.email);
+    }
 }
