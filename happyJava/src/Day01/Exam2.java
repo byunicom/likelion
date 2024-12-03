d73e0e323f10a1e89394b5ebd7a6fd3bc08c0798
byunicom <byunicom@gmail.com>
2024-12-03 09:08:00 +0900

    Day01 * 샘플 소스

--- happyJava/src/Day01/Exam2.java
+++ happyJava/src/Day01/Exam2.java
@@ -3,7 +3,7 @@ package Day01;
 public class Exam2 {
     public static void main(String[] args) {
         String str="";
-        for(int i=0;i<6;i++) {
+        for(int i=0;i<20;i++) {
             str=str+"*";
             System.out.println(str);
         }
