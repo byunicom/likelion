04cc657e9e29e02644abcae83f38349a88fea4dd
byunicom <byunicom@gmail.com>
2024-12-03 13:28:59 +0900

    20241203 Opexam upload

--- happyJava/src/Day002/BitOperatorExam1.java
+++ happyJava/src/Day002/BitOperatorExam1.java
@@ -1,4 +1,16 @@
+/**
+ * 비트연산자 Shift 테스트
+ * */
 package Day002;
 
 public class BitOperatorExam1 {
+    public static void main(String[] args){
+        int a = 4;
+        int b = a >> 1;
+        System.out.println(b);
+
+        int c = 4;
+        int d = c << 1;
+        System.out.println(d);
+    }
 }
--- happyJava/src/Day002/Opexam.java
+++ happyJava/src/Day002/Opexam.java
@@ -1,4 +1,20 @@
+/**
+ * 삼항연산자
+ * */
 package Day002;
 
 public class Opexam {
+    public static void main(String[] args) {
+        //삼항 연산자
+        //(조건)? 식1 : 식2
+        int num = 10;
+        boolean b = (num > 5)?true:false;
+
+        System.out.println(num);
+        System.out.println("-----------------------");
+
+        String result = (num > 0)?"양수":"음수";
+
+        System.out.println(result);
+    }
 }
