e75fec73fc9e8c0d5c43ea853a23ad6f3a37aa43
byunicom <byunicom@gmail.com>
2024-12-03 13:53:35 +0900

    삼항연산자 테스트

--- happyJava/src/Day002/IfExam2.java
+++ happyJava/src/Day002/IfExam2.java
@@ -4,6 +4,7 @@ public class IfExam2 {
     public static void main(String[] args) {
         int i = 15;
 
+        //if문
         if(i>8 && i<=12){
             System.out.println("아침 먹었니?");
         }else if(i>12 && i<=17){
@@ -13,5 +14,11 @@ public class IfExam2 {
         }else{
             System.out.println("야식 먹었니?");
         }
+
+        //삼항연산자
+        int j = 0;
+        String result  = (j == 0)?"A":"B";
+        System.out.println(result);
+
     }
 }
