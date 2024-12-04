370d1c4c94c6c448299ad4b0a728b993f493edb0
byunicom <byunicom@gmail.com>
2024-12-04 13:08:52 +0900

    exam upload2

---
+++ happyJava/src/Day003/StringExam.java
@@ -0,0 +1,15 @@
+package Day003;
+
+public class StringExam {
+    public static void main(String[] args) {
+        String str = "hello";
+        int i = 10;
+        char a = 'a';
+        double d = 5.0;
+        System.out.println(str+i+i);
+        System.out.println("tset"+(30+3));
+        System.out.println(str+a+d+i);
+        System.out.println(""+i);
+        String s = i + "";
+    }
+}
