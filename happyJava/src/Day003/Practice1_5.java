b69361f0035561f8111f6c175fda86a84516a301
byunicom <byunicom@gmail.com>
2024-12-05 11:33:31 +0900

    upload

--- happyJava/src/Day003/Practice1_5.java
+++ happyJava/src/Day003/Practice1_5.java
@@ -7,43 +7,50 @@
  */
 package Day003;
 
+import java.util.Arrays;
+
 public class Practice1_5 {
     public static void main(String[] args) {
         int[] array1 = {1, 3, 5, 7, 9};
         int[] array2 = {0, 2, 4, 6, 8, 10, 3, 5};
 
+
         int totLength = array1.length+array2.length;
-        int totIdx = 0;
+//        int totIdx = 0;
         int[] array3 = new int[totLength];
 
-        for(int i=0;i<array2.length;i++){
-            for(int j=0;j<array1.length;j++){
-                if(array2[i]==array1[j]){
-                    totIdx += totIdx;
-                    array3 = new int[totIdx];
-
-                    System.out.println(array2[i]);
-                }
-            }
-        }
-//        int endIdx=0;
-//        for(int i=0;i<array1.length;i++){
-//            array3[i]=array1[i];
-//            endIdx=i;
-//        }
-//
-//        endIdx++;
-//
 //        for(int i=0;i<array2.length;i++){
+//            array3[i]=array2[i];
+//            for(int j=0;j<array1.length;j++){
+//                if(array2[i]==array1[j]){
+////                    totIdx += totIdx;
+////                    array3 = new int[totIdx];
 //
-//            array3[endIdx]=array2[i];
-//
-//            endIdx++;
+//                    //System.out.println(array2[i]);
+//                    array3[i] = array1[i];
+//                }
+//            }
 //        }
-//        for(int i=0;i<array3.length;i++){
-//            System.out.println(array3[i]);
-//        }
+        int final_idx=0;
+        for(int i=0;i<array1.length;i++){
+            array3[i]=array1[i];
+            final_idx=i;
+        }
+        final_idx++;
+        for(int i=0;i<array2.length;i++){
+            array3[final_idx]=array2[i];
+            final_idx++;
+        }
 
+
+//        System.out.println(array3.length);
+        int[] distArry = Arrays.stream(array3).distinct().toArray(int[]::new);
+        for(int i=0;i<array3.length;i++){
+            System.out.println(array3[i]);
+        }
+
+
+//        System.out.println("333---"+Arrays.stream(array3).distinct().toArray().length);
 
     }
 }
---
+++ happyJava/src/Day004/Exam4.java
@@ -0,0 +1,27 @@
+package Day004;
+
+class Pen{
+    private String name;
+
+    public void write(String msg){
+        System.out.println(msg);
+
+    }
+}
+
+public class Exam4 {
+
+
+    public static void main(String[] args) {
+        //class Pen 은 단지 설계도, 틀
+        //class를 이용해서 실체(진짜 펜) 을 만들어야 함.
+
+        Pen p = new Pen();
+
+        //p 레퍼런스 변수 int[] iarr  - iarr는 레퍼런스 변수
+
+        int i=0;    //int를 담을 수 있는 변수 i 를 선언했고, int 값 0 을 넣어줌.
+        //Pen p -- Pen 을 가르킬 수 있는 변수 p 를 선언.
+    }
+
+}
